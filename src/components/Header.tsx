88a2cdd65ce33d49ed51aa0da6e7f2c15fa669db
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-10-15 11:18:48 -0300

    feat: header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import igniteLogo from "../assets/ignite-logo.svg";
 export function Header() {
   return (
     <header className={styles.header}>
-      <img className={styles.logoSpin} src={igniteLogo} alt="Ignite" />
+      <img src={igniteLogo} alt="Ignite" />
       <strong>Ignite Feed</strong>
     </header>
   );
